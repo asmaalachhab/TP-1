45654b577eb2335137870092ba484c46e7425e46
ASMAA LACHHAB <asmaalachhab1@gmail.com>
2025-11-17 10:52:34 +0100

    Refactor DBConnection.java by removing comments
    Removed comments and documentation related to JDBC connection.

--- src/com/projet/capteur/serveur/DBConnection.java
+++ src/com/projet/capteur/serveur/DBConnection.java
@@ -4,10 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Classe utilitaire pour obtenir une connexion JDBC vers la base MySQL.
- * Modifiez USER et PASSWORD si nécessaire.
- */
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/capteurs_db?serverTimezone=UTC&useSSL=false";
     private static final String USER = "root";
@@ -15,7 +11,6 @@ public class DBConnection {
 
     static {
         try {
-            // Charger le driver MySQL (souvent pas nécessaire si driver sur classpath)
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.err.println("Driver JDBC MySQL introuvable. Assurez-vous que le connector est sur le classpath.");
